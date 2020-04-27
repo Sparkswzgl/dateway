af7fa9ee61a6659418639443722443151443bab1
王兵兵_集团 <wangbingbing@longfor.com>
2020-04-27 15:52:52 +0800

    the project of dataway test01

---
+++ src/main/java/net/example/hasor/ExampleModule.java
@@ -0,0 +1,30 @@
+package net.example.hasor;
+
+import net.hasor.core.ApiBinder;
+import net.hasor.core.DimModule;
+import net.hasor.db.JdbcModule;
+import net.hasor.db.Level;
+import net.hasor.spring.SpringModule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+/**
+ * @Author: wangbingbing
+ * @Description:
+ * @Date: Create in 17:21 2020/4/24
+ */
+
+@DimModule
+@Component
+public class ExampleModule implements SpringModule {
+
+    @Autowired
+    private DataSource dataSource = null;
+
+    @Override
+    public void loadModule(ApiBinder apiBinder) throws Throwable {
+        apiBinder.installModule(new JdbcModule(Level.Full, this.dataSource));
+    }
+}
---
+++ src/test/java/com/example/dataway/DatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
